c4d08e146cad626fc62dc68c429d4f379f6f8b3d
Yida Chen <yc015@bucknell.edu>
2022-11-18 19:34:09 -0500

    a workable interface

---
+++ app.py
@@ -0,0 +1,181 @@
+# The main app of this flask todo app
+# The code are adapted from the source code on https://www.python-engineer.com/posts/flask-todo-app/ by Python Engineer
+
+# Import flask and SQLAlchemy library
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from lang_viz_interp_generator import main as generate_tokens
+import os
+
+import argparse
+
+import random
+
+args = argparse.Namespace()
+args.lang_model = "gpt2"
+args.random_state = 0
+args.saliency = True
+args.device = "cuda"
+args.saliency_metric = "mean"
+args.attn_layer_sel = "attn_layer_11"
+args.agg_method = "mean"
+args.num_tokens = 10
+args.num_tokens_buffed = 5
+args.attn_pairs = True
+args.return_output = True
+args.do_sample = False
+
+# Create the flask with static folder name static in the same level of directory with the app.py
+app = Flask(__name__, static_folder='./static')
+
+
+# Configure the SQL database for the app
+# Specify the location of the SQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+# Don't track modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Conifgure the SQL database with above specifications
+db = SQLAlchemy(app)
+
+keyword_pairs = None
+current_output_full_text = "Write your prompt here!"
+
+
+# Model class for the Todo table
+class Tokens(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(100))
+    saliency = db.Column(db.String(100))
+    intext_index = db.Column(db.Integer)
+    token_len = db.Column(db.Integer)
+    keyword_ind = db.Column(db.Integer)
+
+
+# API route for generating a new token
+@app.route("/generate", methods=["POST"])
+def generate():
+    global keyword_pairs, current_output_full_text
+    Tokens.query.delete()
+    # Get the description of todo task from the user submitted form
+    # and use it as the todo item's title in the database
+    num_tokens = request.form.get("numTokens")
+    if len(num_tokens) > 0:
+        try:
+            num_tokens = int(num_tokens)
+        except:
+            num_tokens = 10
+    else:
+        num_tokens = 10
+    input_text = request.form.get("textInput")
+
+    layer_sel = request.form.get("attentionLayer")
+    if request.form.get("random_state") == "deterministic":
+        print(args.random_state)
+        args.random_state = 0
+        args.do_sample = False
+    elif request.form.get("random_state") == "random":
+        args.random_state = random.randint(0, 1e8)
+        args.do_sample = True
+
+    if layer_sel == "":
+        layer_sel = "12"
+    layer_sel = int(layer_sel) - 1
+    args.attn_layer_sel = f"attn_layer_{layer_sel:d}"
+    
+    args.prompt = input_text
+    args.num_tokens = int(num_tokens)
+
+    output = generate_tokens(args)
+    keyword_pairs = output["key_attn_pairs_ind"]
+    keyword_ind_map = {}
+    for keyword_pair in keyword_pairs:
+        keyword_ind_map[keyword_pair[-1]] = keyword_pair[-2]
+
+
+    for i in range(len(output["input_tokens"])):
+        token_text = output["input_tokens"][i]
+        index = i 
+        saliency = ""
+        token_len = len(token_text)
+
+        if index in keyword_ind_map.keys():
+            keyword_ind = keyword_ind_map[index]
+        else:
+            keyword_ind = -1 
+
+        new_token = Tokens(token=token_text, saliency=saliency, 
+                           intext_index=index, token_len=token_len,
+                           keyword_ind=keyword_ind)
+
+        db.session.add(new_token)
+        # Commit the change
+        db.session.commit()
+    cur_index = i
+
+    for i in range(len(output["tokens"])):
+        token_text = output["tokens"][i]
+        saliency = "".join([f"{score:.2f}," for score in output["saliency"][i]])
+        index = cur_index + i
+        token_len = len(token_text)
+
+        if index in keyword_ind_map.keys():
+            keyword_ind = keyword_ind_map[index]
+        else:
+            keyword_ind = -1 
+
+        new_token = Tokens(token=token_text, saliency=saliency, 
+                           intext_index=index, token_len=token_len,
+                           keyword_ind=keyword_ind)
+
+        db.session.add(new_token)
+        # Commit the change
+        db.session.commit()
+    
+    current_output_full_text = output["output_full_text"]
+
+    # Redirect to the home page with updated todo list
+    return redirect(url_for("home"))
+
+
+# API route for directing to the main page
+@app.route("/")
+def home():
+    # Get all stored todo items
+    token_list = Tokens.query.all()
+    if len(token_list) > 0:
+        for i in range(len(token_list)):
+            if len(token_list[i].saliency) > 0:
+                saliency_scores = token_list[i].saliency[:-1].split(",")
+                saliency_scores = [float(score) for score in saliency_scores]
+                min_score = min(saliency_scores)
+                saliency_scores = [score - min_score for score in saliency_scores]
+                max_score = max(saliency_scores)
+                saliency_scores = [score / max_score for score in saliency_scores]
+                scale = 12
+                base = 8
+                saliency_scores = [int(score * scale + base) for score in saliency_scores]
+
+                token_list[i].saliency = ("".join([f"{score:d}," for score in saliency_scores]))[:-1]
+                # token_list[i].saliency = "12"
+                # token_list[i].token_len = len(token_list[i].token)
+    # Display them in the main page
+
+    num_tokens = args.num_tokens
+    sel_attn_ind = int(args.attn_layer_sel[args.attn_layer_sel.rfind("_")+1:]) + 1
+    random = "checked" if args.do_sample else ""
+    deterministic = "checked" if not args.do_sample else ""
+
+    return render_template("base.html", token_list=token_list, current_output_full_text=current_output_full_text,
+                            num_tokens=num_tokens, sel_attn_ind=sel_attn_ind, deterministic=deterministic, random=random)
+
+
+# When run this file call the functions below
+if __name__ == "__main__":
+    # Within the context of this application
+    with app.app_context():
+        # Create the SQL database
+        db.create_all()
+    # Find the port defined in the environment (if not exist use default port 5000)
+    port = int(os.environ.get('PORT', 5000))
+    # Run the app on the local host with assigned port
+    app.run(host='0.0.0.0', port=port)
---
+++ lang_viz_interp_generator.py
@@ -0,0 +1,142 @@
+import torch
+from mingpt.model import GPT
+from mingpt.utils import set_seed
+from mingpt.bpe import BPETokenizer
+
+import numpy as np
+import argparse
+
+import json
+
+from config import getConfig
+
+use_mingpt = True # Let's just use mingpt now. We can switch to huggingface's GPT model later if we think it's necessary
+
+
+def generate(prompt, tokenizer, model, num_samples=1, steps=20, do_sample=False, saliency=False):    
+    x = tokenizer(prompt).to(args.device)
+    # we'll process all desired num_samples in a batch, so expand out the batch dim
+    x = x.expand(num_samples, -1)
+
+    # forward the model `steps` times to get samples, in a batch
+    # For consistency reason, do not sample output logits. We want results to be reproducible
+    output = model.generate(x, max_new_tokens=steps, do_sample=do_sample, 
+                            return_logits=saliency, save_grad=saliency)
+    
+    y = output[0]
+    interm_output = output[1:]
+    
+    out = tokenizer.decode(y[0].cpu().squeeze())
+
+    return y, out, interm_output
+
+
+def main(args):
+    set_seed(args.random_state)
+    
+    output_dict = {"input_prompt": args.prompt}
+    
+    out = args.prompt
+    
+    input_tokens = tokenizer(args.prompt)
+    input_tokens = [tokenizer.decode(tok.unsqueeze(0)) for tok in input_tokens[0]]
+    output_dict["input_tokens"] = input_tokens
+    for i in range(args.num_tokens):
+        y, out, interm_output = generate(prompt=out,
+                                         tokenizer=tokenizer, 
+                                         model=model,
+                                         num_samples=1, 
+                                         steps=1,
+                                         do_sample=args.do_sample,
+                                         saliency=args.saliency)
+
+        token = tokenizer.decode(y[0][-1].unsqueeze(0))
+        
+        if "tokens" not in output_dict.keys():
+            output_dict["tokens"] = []
+        output_dict["tokens"].append(token)
+
+        if args.saliency:
+            logits = interm_output[1]
+            tok_emb = interm_output[2]
+            grads = torch.autograd.grad(tuple(torch.flatten(logits[..., y[0][-1]])), tok_emb, retain_graph=True)
+            aggregated_grads = []
+            for grad, inp in zip(grads, tok_emb):
+                assert args.saliency_metric in ["mean", "inputXGrad"]
+                if args.saliency_metric == "inputXGrad":
+                    aggregated_grads.append(torch.norm(grad * inp, dim=-1))
+                elif args.saliency_metric == "mean":
+                    aggregated_grads.append(grad.abs().mean(-1))
+                
+            if "saliency" not in output_dict.keys():
+                output_dict["saliency"] = []
+            output_dict["saliency"].append(aggregated_grads[0].cpu().detach()[0].tolist())
+        
+    output_dict["output_full_text"] = out
+    output_dict["output_token_list"] = [tokenizer.decode(token.unsqueeze(0)) for token in y[0]]
+    
+    if args.attn_pairs:
+        attn = interm_output[0]
+        output_dict["key_attn_pairs"] = {}
+        output_dict["key_attn_pairs_ind"] = []
+        if args.agg_method == "max":
+            agg_attn = torch.amax(attn[args.attn_layer_sel][0], dim=0).cpu().detach()[1:, 1:]
+        elif args.agg_method == "mean":
+            agg_attn = torch.mean(attn[args.attn_layer_sel][0], dim=0).cpu().detach()[1:, 1:]
+            
+        for i in range(agg_attn.shape[1]):
+            key_ind = torch.argmax(agg_attn[i])
+            if key_ind == i or i < args.num_tokens_buffed:
+                continue
+            iqr = torch.quantile(agg_attn[i][:i+1], 0.75) - torch.quantile(agg_attn[i][:i+1], 0.25)
+            if 1.5 * iqr + torch.quantile(agg_attn[i][:i+1], 0.75) > agg_attn[i][key_ind]:
+            # if 2 * torch.mean(agg_attn[i][:i+1]) > agg_attn[i][key_ind]:
+                continue
+            
+            key_word = output_dict["output_token_list"][key_ind+1].strip(" ")
+            cur_word = output_dict["output_token_list"][i+1].strip(" ")
+            
+            if not (cur_word.isalpha() and key_word.isalpha()):
+                continue
+  
+            key_word_ind = key_word + f"_{int(key_ind.cpu()+1)}"
+            cur_word_ind = cur_word + f"_{i+1}"
+            if key_word_ind not in output_dict["key_attn_pairs"].keys():
+                output_dict["key_attn_pairs"][key_word_ind] = []
+            output_dict["key_attn_pairs"][key_word_ind].append(cur_word_ind)
+            
+            output_dict["key_attn_pairs_ind"].append((key_word, cur_word, 
+                                                  int(key_ind.cpu() + 1), i + 1))
+        
+    if args.return_output:
+        return output_dict
+    else:
+        with open(args.out_path, "w") as outfile:
+            json.dump(output_dict, outfile)
+
+if __name__ == '__main__':
+    args = getConfig()
+    if args.device == "Default":
+        args.device = "cuda" if torch.cuda.is_available() else "cpu"
+    main(args)    
+else:
+    args = argparse.Namespace()
+    args.lang_model = "gpt2"
+    args.random_state = 0
+    args.saliency = True
+    args.device = "cuda" if torch.cuda.is_available() else "cpu"
+    args.saliency_metric = "mean"
+    args.attn_layer_sel = "attn_layer_12"
+    args.agg_method = "mean"
+    args.num_tokens = 10
+    args.num_tokens_buffed = 5
+    args.attn_pairs = True
+    args.return_output = True
+    args.do_sample = False
+    model = GPT.from_pretrained(args.lang_model)
+
+    # ship model to device and set to eval mode
+    model.to(args.device)
+    model.eval()
+    tokenizer = BPETokenizer()
+    
